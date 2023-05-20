cabea2a3709ab15ad35b709645e1d160d3d6962b
Joaquin Jordan <joaquin.jordan@utec.edu.pe>
2023-05-20 16:10:55 -0500

    implemented join command

---
+++ InteractionHandler/bot.js
@@ -0,0 +1,20 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const commands = require('./commands.js');
+require('dotenv').config();
+
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on(Events.InteractionCreate, (interaction) => {
+	commands.execute(interaction)
+		.then(result => console.log('Command returned this', result))
+		.catch(err => console.error('Command failed', err));
+});
+
+client.login(process.env.TOKEN);
