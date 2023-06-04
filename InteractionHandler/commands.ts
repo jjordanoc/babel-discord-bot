6cf551f1c06c8624b72773f683baf3657605920d
NicolasArroyo <nicolas.arroyo.c@utec.edu.pe>
2023-06-03 22:48:06 -0500

    fix: bot now translates user who made /join

--- InteractionHandler/commands.ts
+++ InteractionHandler/commands.ts
@@ -57,7 +57,7 @@ async function joinHandler(interaction) {
 		const receiver = connection.receiver;
 		receiver.speaking.once('start', (callerUserId) => {
 			console.log('User started speaking. Called event once.');
-			createListeningStream(receiver, callerUserId, interaction.client.users.cache.get(callerUserId));
+			createListeningStream(receiver, interaction.user.id, interaction.client.users.cache.get(interaction.user.id));
 		});
 		interaction.reply({ ephemeral: true, content: 'Listening...' });
 	}
