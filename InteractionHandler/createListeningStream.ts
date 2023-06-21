da40604791660bbdfd01fe287f5611a81702f126
Joaquin Jordan <joaquin.jordan@utec.edu.pe>
2023-06-21 13:06:15 -0500

    fix: added voice queue to avoid overlapping audios

--- InteractionHandler/createListeningStream.ts
+++ InteractionHandler/createListeningStream.ts
@@ -1,19 +1,24 @@
-import { createAudioPlayer, createAudioResource, EndBehaviorType, StreamType, VoiceConnection } from '@discordjs/voice';
+import {
+	AudioPlayerStatus,
+	createAudioPlayer,
+	createAudioResource,
+	EndBehaviorType,
+	StreamType,
+	VoiceConnection,
+} from '@discordjs/voice';
 import * as prism from 'prism-media';
 import { WebSocket } from 'ws';
 import * as stream from 'stream';
 import * as JSON from 'json3';
 
+
 export function createListeningStream(connection: VoiceConnection, userId: string, languages: {
 	'source': string,
 	'target': string
 }) {
-	const player = createAudioPlayer();
-	player.on('error', error => {
-		console.error(error.message);
-	});
-	connection.subscribe(player);
-	console.log('Reached 1');
+	/*
+	Send user audio through socket
+	 */
 	const opusStream = connection.receiver.subscribe(userId, {
 		end: {
 			behavior: EndBehaviorType.Manual,
@@ -21,8 +26,6 @@ export function createListeningStream(connection: VoiceConnection, userId: strin
 	});
 	opusStream.on('end', () => {
 		// TODO: perform cleanup
-		console.log('Ended user audio stream');
-		audioSocket.close();
 	});
 	opusStream.on('close', () => {
 		console.log('Closed user audio stream');
@@ -45,20 +48,38 @@ export function createListeningStream(connection: VoiceConnection, userId: strin
 			audioSocket.close();
 		});
 	});
-	console.log('Reached 2');
-	audioSocket.on('close', () => {
-		// clean up
-	});
-	audioSocket.on('message', (data) => {
-		const translatedAudioStream = new stream.PassThrough();
-		console.log(data);
-		translatedAudioStream.write(data);
-		player.play(createAudioResource(translatedAudioStream, { inputType: StreamType.OggOpus }));
-		translatedAudioStream.end();
-	});
-	console.log('Reached 3');
 	audioSocket.on('error', () => {
 		console.error('An error occurred while connecting to WebSocket');
 	});
-	console.log('Reached 4');
+	/*
+	Play received audio
+	 */
+	const player = createAudioPlayer();
+	player.on('error', error => {
+		console.error(error.message);
+	});
+	connection.subscribe(player);
+	const queue = [];
+	player.on('stateChange', (oldState, newState) => {
+		if (oldState.status == AudioPlayerStatus.Playing
+			&& newState.status == AudioPlayerStatus.Idle
+			&& queue.length > 0) {
+			// play next resource in queue
+			player.play(queue.shift());
+		}
+	});
+	audioSocket.on('message', (data) => {
+		const translatedAudioStream = new stream.PassThrough();
+		translatedAudioStream.write(data);
+		translatedAudioStream.end();
+		const resource =
+			createAudioResource(translatedAudioStream, { inputType: StreamType.OggOpus });
+		if (player.state.status == AudioPlayerStatus.Playing) {
+			// player is currently busy, add to queue
+			queue.push(resource);
+		}
+		else {
+			player.play(resource);
+		}
+	});
 }
