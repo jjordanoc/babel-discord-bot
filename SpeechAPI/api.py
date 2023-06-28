de5c1b4ab68812694b2265f53ca3138508c7d396
Joaquin Jordan <joaquin.jordan@utec.edu.pe>
2023-06-28 12:59:27 -0500

    fix: added connection keep alive task, refactored some code and removed global variable in module scope

--- SpeechAPI/api.py
+++ SpeechAPI/api.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 import os
 
 import openai
@@ -29,7 +30,6 @@ openai.api_key = os.getenv("GPT_KEY")
 
 def gpt3_request(prompt, model="gpt-3.5-turbo-0613"):
     messages = [
-        # {"role": "system", "content": "Responde lo siguiente en 50 palabras o menos:"},
         {"role": "user", "content": prompt}
     ]
 
@@ -47,15 +47,12 @@ def gpt3_request(prompt, model="gpt-3.5-turbo-0613"):
                 current_sentence = ''
 
 
-main_gpt_flag = False
-
-
 @app.websocket("/")
 async def audio_socket(websocket: WebSocket):
+    main_gpt_flag = False
     await websocket.accept()
     # Receive the source language and the target language
     languages = await websocket.receive_json()
-    print(languages)
     src_lang = languages["source"]
     trg_lang = languages["target"]
     gender_lang = languages["gender"]
@@ -70,14 +67,17 @@ async def audio_socket(websocket: WebSocket):
         print(f'Could not open socket: {e}')
         await websocket.close(code=500)
         return
-    print('websocket.accept')
 
     # Listen for the connection to close
-    deepgram_live.registerHandler(deepgram_live.event.CLOSE, lambda _: print('Connection closed.'))
+    deepgram_live.registerHandler(deepgram_live.event.CLOSE, lambda _: print('Connection with deepgram closed.'))
 
-    # Listen for any transcripts received from Deepgram and write them to the console (translate them)
+    """
+    Listen for any transcripts received from Deepgram
+    and use them for translation, querying or playing music
+    """
+
     async def translate_and_send_audio_chunk(result):
-        global main_gpt_flag
+        nonlocal main_gpt_flag
         print("MAIN_GPT_FLAG 1 = " + str(main_gpt_flag))
         print(result)
         if "is_final" in result and result["is_final"]:
@@ -99,28 +99,8 @@ async def audio_socket(websocket: WebSocket):
                 print("Empty transcription")
                 return
 
-            response = translate_client.translate_text(
-                request={
-                    "parent": parent,
-                    "contents": [transcription],
-                    "mime_type": "text/plain",
-                    "source_language_code": src_lang,
-                    "target_language_code": trg_lang,
-                }
-            )
-
-            if response.translations:
-                translation = response.translations[0].translated_text
-            else:
-                print(f"Error in translation {response}")
-                return
-
-            azure_url = f"""https://{os.getenv("SPEECH_REGION")}.tts.speech.microsoft.com/cognitiveservices/v1"""
-
-            azure_headers = {"Ocp-Apim-Subscription-Key": F"""{os.getenv("SPEECH_KEY")}""",
-                             "Content-type": "application/ssml+xml",
-                             "X-Microsoft-OutputFormat": "ogg-48khz-16bit-mono-opus"}
-
+            # If the user is making a voice query, the speaker's voice should be in the user's source language
+            speaker = ""
             if main_gpt_flag:
                 if src_lang == "en":
                     if gender_lang == "Female":
@@ -147,6 +127,7 @@ async def audio_socket(websocket: WebSocket):
                         speaker = "de-DE-AmalaNeural"
                     elif gender_lang == "Male":
                         speaker = "de-DE-KasperNeural"
+            # Otherwise, the translation should be spoken in the target language
             else:
                 if trg_lang == "en":
                     if gender_lang == "Female":
@@ -174,45 +155,79 @@ async def audio_socket(websocket: WebSocket):
                     elif gender_lang == "Male":
                         speaker = "de-DE-KasperNeural"
 
+            azure_url = f"""https://{os.getenv("SPEECH_REGION")}.tts.speech.microsoft.com/cognitiveservices/v1"""
+
+            azure_headers = {"Ocp-Apim-Subscription-Key": F"""{os.getenv("SPEECH_KEY")}""",
+                             "Content-type": "application/ssml+xml",
+                             "X-Microsoft-OutputFormat": "ogg-48khz-16bit-mono-opus"}
+
             if main_gpt_flag:
                 for sentence in gpt3_request(transcription):
                     translation = sentence
                     translation = translation.encode('ascii', 'xmlcharrefreplace').decode("ascii")
-                    azure_body = f"""<speak version='1.0' xml:lang='es-ES'><voice xml:lang='es-ES' xml:gender='{gender_lang}' name='{speaker}'>{translation}</voice></speak>"""
+                    azure_body = f"""<speak version='1.0' xml:lang="en-US"><voice xml:lang="en-US" xml:gender='{gender_lang}' name='{speaker}'>{translation}</voice></speak>"""
                     response_azure = req.post(azure_url, headers=azure_headers, data=azure_body)
                     audio = response_azure.content
                     await websocket.send_bytes(audio)
-
                 main_gpt_flag = False
-
             else:
+                # Translate transcription
+                google_translate_response = translate_client.translate_text(
+                    request={
+                        "parent": parent,
+                        "contents": [transcription],
+                        "mime_type": "text/plain",
+                        "source_language_code": src_lang,
+                        "target_language_code": trg_lang,
+                    }
+                )
+
+                if google_translate_response.translations:
+                    translation = google_translate_response.translations[0].translated_text
+                else:
+                    print(f"Error in translation {google_translate_response}")
+                    return
                 # Fix unrecognizeable characters
                 translation = translation.encode('ascii', 'xmlcharrefreplace').decode("ascii")
-                azure_body = f"""<speak version='1.0' xml:lang='es-ES'><voice xml:lang='es-ES' xml:gender='{gender_lang}' name='{speaker}'>{translation}</voice></speak>"""
-                response_azure = req.post(azure_url, headers=azure_headers, data=azure_body)
-                audio = response_azure.content
+                azure_request_body = f"""<speak version='1.0' xml:lang="en-US"><voice xml:lang="en-US" xml:gender='{gender_lang}' name='{speaker}'>{translation}</voice></speak>"""
+                azure_text_to_speech_response = req.post(azure_url, headers=azure_headers, data=azure_request_body)
+                audio = azure_text_to_speech_response.content
                 main_gpt_flag = False
-
                 await websocket.send_bytes(audio)
 
     deepgram_live.registerHandler(deepgram_live.event.TRANSCRIPT_RECEIVED, translate_and_send_audio_chunk)
 
+    """
+    Receive the audio asynchronously and send it to deepgram
+    """
+
     async def receive_audio():
         while True:
-            # RECEIVE
             audio_bytes: bytes = await websocket.receive_bytes()
             deepgram_live.send(audio_bytes)
 
-    receive_task = asyncio.create_task(receive_audio())
+    """
+    Make sure the connection is kept alive by
+    sending a KeepAlive JSON to Deepgram every 7 seconds
+    """
+
+    async def keep_deepgram_alive():
+        while True:
+            deepgram_live.send(json.dumps({
+                "type": "KeepAlive"
+            }))
+            await asyncio.sleep(7)
+
+    receive_audio_task = asyncio.create_task(receive_audio())
+    keep_deepgram_alive_task = asyncio.create_task(keep_deepgram_alive())
 
     try:
-        await asyncio.gather(receive_task)
+        await asyncio.gather(receive_audio_task, keep_deepgram_alive_task)
     except Exception as e:
-        print("Error in socket:", e)
+        print("Error in websocket:", e)
     finally:
         await deepgram_live.finish()
-
-    print('leave websocket_endpoint')
+    print('Websocket connection closed')
 
 
 if __name__ == '__main__':
